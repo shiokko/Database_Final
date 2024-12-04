b555b778bf00ec688c534660986257af7cf37264
evan1223 <evanho8040@gmail.com>
2024-12-04 15:21:16 +0800

    update README.md for Database & instructions

---
+++ Database/create_table.sql
@@ -0,0 +1,57 @@
+-- Create Users table
+CREATE TABLE Users (
+    u_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL
+);
+
+-- Create Blacklist table
+CREATE TABLE Blacklist (
+    b_id INTEGER NOT NULL,
+    u_id INTEGER NOT NULL,
+    date DATETIME NOT NULL,
+    PRIMARY KEY (b_id, u_id),
+    FOREIGN KEY (u_id) REFERENCES Users (u_id)
+);
+
+-- Create Area table
+CREATE TABLE Area (
+    a_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    a_name TEXT NOT NULL
+);
+
+-- Create Restaurant table
+CREATE TABLE Restaurant (
+    r_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    r_name TEXT NOT NULL,
+    a_id INTEGER NOT NULL,
+    FOREIGN KEY (a_id) REFERENCES Area (a_id)
+);
+
+-- Create Type table
+CREATE TABLE Type (
+    t_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    t_name TEXT NOT NULL
+);
+
+-- Create Restaurant_Types table
+CREATE TABLE Restaurant_Types (
+    r_id INTEGER NOT NULL,
+    t_id INTEGER NOT NULL,
+    PRIMARY KEY (r_id, t_id),
+    FOREIGN KEY (r_id) REFERENCES Restaurant (r_id),
+    FOREIGN KEY (t_id) REFERENCES Type (t_id)
+);
+
+-- Create History table
+CREATE TABLE History (
+    h_id INTEGER NOT NULL,
+    u_id INTEGER NOT NULL,
+    r_id INTEGER NOT NULL,
+    Rate REAL NOT NULL,
+    Reviews TEXT,
+    Date DATETIME NOT NULL,
+    PRIMARY KEY (h_id, u_id),
+    FOREIGN KEY (u_id) REFERENCES Users (u_id),
+    FOREIGN KEY (r_id) REFERENCES Restaurant (r_id)
+);
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-DBMS Project
+# DBMS Project
+
+
+### About Database 
+`create_table.sql` is the raw SQL commands to create seven tables of the project.(for backup)
+`project.db` is the created Database.
+Enter the Database by using the command `sqlite3 project.db`.
+
