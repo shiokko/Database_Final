58dc38a31b11786eb8223686cb38739b024b307a
evan1223 <evanho8040@gmail.com>
2024-12-19 13:48:51 +0800

    adding navigation bar for user & modified elegant user backend using session

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'key'  # this is a requirement for using session
+
+
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    
+def login():
     if request.method == 'POST':
         name = request.form['username']
-        print(name)
-        return redirect(url_for('welcome', username=name))
-    
+        session['username'] = name  # Store username in session
+        return redirect(url_for('welcome'))
+
     return render_template('index.html')
 
-@app.route('/welcome/<username>')
-def welcome(username):
+
+@app.route('/home')
+def welcome():
+    username = session.get('username')      # Fetch username from session
+    if not username:
+        return redirect(url_for('login'))  # Redirect to login if not logged in
     return render_template('home.html', username=username)
+
+
+@app.route('/home/history')
+def history():
+    username = session.get('username')  # Fetch username from session
+    if not username:
+        return redirect(url_for('login'))
+    return render_template('history.html', username=username)
+
+
+@app.route('/home/rating')
+def rating():
+    username = session.get('username')  # Fetch username from session
+    if not username:
+        return redirect(url_for('login'))
+    return render_template('rating.html', username=username)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()  # Clear session on logout
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
